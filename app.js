6b13ee33332aa98c92d2ea122a01438422b653bd
Hydra1War <hydra1war@gmail.com>
2023-09-13 15:42:12 +0300

    попытка сделать switcher

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ function clearActiveClasses() {
     slide.classList.remove('active')
   })
 }
+const body = document.querySelector('body')
+document.querySelector("#button_switch_mode").onclick = function(){
+  body.classList.add('black_mode')
+}
+
+document.querySelector("#button_switch_mode").onclick = function(){
+  if (body.hasClass('black_mode')){
+    body.classList.remove('black_mode')
+  }
+}
